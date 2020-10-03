4531d020e099ca9e9c456100c52c5e9c28f5c9bf
Ariyo <alokhalder6359@gmail.com>
2020-10-03 14:57:25 +0530

    minnor changes

--- home/views.py
+++ home/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from .models import Event
 import datetime
-from math import ceil
+from django.core.mail import send_mail
 
 mydate = datetime.date.today
 
@@ -21,7 +21,25 @@ def events(request):
     params={ 'Event':event, 'tday':mydate}
     return render(request, "home/events.html", params)
 def contact(request):
-    
+    if request.method=="POST":
+        name=request.POST.get('Name','')
+        email=request.POST.get('Email', '')
+        phone=request.POST.get('Phone','')
+        message=request.POST.get('Message', '')
+        try:
+            send_mail(
+            'Bikerider contact message',
+            f"Phone: {phone} \n Message: {message}",
+            email,
+            ['alokhalder6359@gmail.com'],
+            fail_silently=False,
+            ) 
+        except Exception as e:
+            print(e)
+            return HttpResponseRedirect('/')  
+            
+        return HttpResponseRedirect('/')  
+        
     return render(request, "home/contact.html")
 
 def event_read(request, myid):
